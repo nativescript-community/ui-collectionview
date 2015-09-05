451c236fe839b09e9a25b39c9db53134951c0083
Peter Staev <peter@tangrasoft.com>
2015-09-05 16:26:23 +0300

    VS Project

--- grid-view-common.ts
+++ grid-view-common.ts
@@ -1,6 +1,22 @@
+/*! *****************************************************************************
+Copyright (c) 2015 Tangra Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+***************************************************************************** */
+
 import observable = require("data/observable");
 import proxy = require("ui/core/proxy");
-import definition = require("./grid-view");
+import definition = require("nativescript-grid-view");
 import dependencyObservable = require("ui/core/dependency-observable");
 import builder = require("ui/builder");
 import view = require("ui/core/view");
@@ -135,7 +151,6 @@ export class GridView extends view.View implements definition.GridView
         );
 
     private _itemsChanged: (args: observable.EventData) => void;
-    public _setCurrentMeasureSpecs: (widthSpec: number, heightSpec: number) => boolean;
 
     constructor()
     {
@@ -197,10 +212,7 @@ export class GridView extends view.View implements definition.GridView
         this._setValue(GridView.horizontalSpacingProperty, value);
     }
 
-    public refresh()
-    {
-        //
-    }
+    public refresh() { }
 
     public _getItemTemplateContent(): view.View
     {
--- grid-view.ios.ts
+++ grid-view.ios.ts
@@ -1,6 +1,22 @@
+/*! *****************************************************************************
+Copyright (c) 2015 Tangra Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+***************************************************************************** */
+
 import observable = require("data/observable");
 import listView = require("ui/list-view");
-import definition = require("./grid-view");
+import definition = require("nativescript-grid-view");
 import common = require("./grid-view-common");
 import utils = require("utils/utils");
 import view = require("ui/core/view");
@@ -148,7 +164,7 @@ export class GridView extends common.GridView
 
         this._layout = new UICollectionViewFlowLayout();
         this._ios = UICollectionView.alloc().initWithFrameCollectionViewLayout(CGRectMake(0, 0, 0, 0), this._layout);
-        this._ios.backgroundColor = [UIColor clearColor];
+        this._ios.backgroundColor = UIColor.clearColor();
         this._ios.registerClassForCellWithReuseIdentifier(GridViewCell.class(), CELLIDENTIFIER);
         this._ios.autoresizesSubviews = false;
         this._ios.autoresizingMask = UIViewAutoresizing.UIViewAutoresizingNone;
@@ -328,4 +344,6 @@ function getNativePaddingLeftValue(gridView: GridView): any
 }
 var paddingLeftChangedHandler = new style.stylers.StylePropertyChangedHandler(setPaddingLeft, resetPaddingLeft, getNativePaddingLeftValue);
 style.stylers.registerHandler(stylingStyle.paddingLeftProperty, paddingLeftChangedHandler, "GridView");
+//#endregion
+
 //#endregion
