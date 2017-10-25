4a1354d642bb546a75c7baf5184b198c65768abf
Peter Staev <peter@tangrasoft.com>
2017-10-25 11:34:42 +0300

    fix windows build

--- gruntfile.js
+++ gruntfile.js
@@ -48,15 +48,9 @@
             }
         },
         exec: {
-            tsCompile: {
-                cmd: "node ./node_modules/typescript/bin/tsc --project tsconfig.json --outDir " + localConfig.outDir
-            },
-            ngCompile: {
-                cmd: "node ./node_modules/.bin/ngc --project tsconfig.aot.json --outDir " + localConfig.outDir
-            },
-            tslint: {
-                cmd: "node ./node_modules/tslint/bin/tslint --project tsconfig.json --type-check"
-            },
+            tsCompile: "npm run tsc -- --outDir " + localConfig.outDir,
+            ngCompile: "npm run ngc -- --outDir " + localConfig.outDir,
+            tslint: "npm run tslint",
             checkRequiredReadmeSection: {
                 cwd: "bin/dist",
                 cmd: function (section) {
