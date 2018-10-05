589f1223e360cc60c8442ccb655a1ecad5e76adb
Martin Guillon <martin.guillon@akylas.fr>
2018-10-05 18:13:20 +0200

    some fixes

--- dev_demo/app/app.ts
+++ dev_demo/app/app.ts
@@ -1,11 +1,8 @@
 import "./bundle-config";
 
 import application = require("application");
-import profiling = require("profiling");
+// import profiling = require("profiling");
 
-
-(global as any).gc();   // Trigger a JavaScript GC pass
-java.lang.System.gc();  // Trigger a Java GC Pass
 
 // profiling.enable('timeline');
 // profiling.start();
