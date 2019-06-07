f1865dbb1ff4d8c9df4cc9d6b946ed8041dcb6fa
Martin Guillon <martin.guillon@akylas.fr>
2019-06-07 19:13:14 +0200

    fix: broken typings

--- src/collectionview.d.ts
+++ src/collectionview.d.ts
@@ -1,1 +1,45 @@
-../plugin/collectionview.d.ts
+
+
+import { KeyedTemplate, PercentLength, Template, View } from "tns-core-modules/ui/core/view";
+import { ItemsSource } from "tns-core-modules/ui/list-view";
+import { EventData } from "tns-core-modules/data/observable";
+
+export type Orientation = "horizontal" | "vertical"
+
+export class CollectionView extends View {
+    public static itemLoadingEvent: string;
+    public static itemTapEvent: string;
+    public static loadMoreItemsEvent: string;
+
+    public items: any[] | ItemsSource;
+    public itemTemplate: string | Template;
+    public itemTemplates: string | KeyedTemplate[];
+    public rowHeight: PercentLength;
+    public colWidth: PercentLength;
+    public orientation: Orientation;
+
+    // public ios: any; /* UICollectionView */
+    // public android: any; /* android.support.v7.widget.RecyclerView */
+
+    public refresh();
+    public scrollToIndex(index: number, animated?: boolean);
+    public onItemTemplatesPropertyChanged(oldValue: any, newValue: any): void;
+    public itemViewLoader?:(viewType)=>any
+}
+
+export interface CollectionViewItemEventData extends EventData {
+    eventName: string;
+    object: CollectionView;
+    index: number;
+    view: View;
+}
+
+/**
+ * Defines the different view types that {@link RadListView} can display in various scenarios.
+ */
+export enum ListViewViewTypes {
+    /**
+     * Identifies a view created using the {@link itemTemplate} value.
+     */
+    ItemView,
+}
