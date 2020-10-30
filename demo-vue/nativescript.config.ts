317cc0c3ab881a03d3fdf1379c545eeccab379ce
Karl Schmaltz <karlschmaltz@gmail.com>
2020-10-30 06:39:31 -0600

    fix: make Vue demo consistent with others

--- demo-vue/nativescript.config.ts
+++ demo-vue/nativescript.config.ts
@@ -3,7 +3,6 @@ import { NativeScriptConfig } from '@nativescript/core';
 export default {
     id: 'org.nativescript.vuedemo',
     appResourcesPath: 'App_Resources',
-    profiling: 'timeline',
     android: {
         v8Flags: '--expose_gc',
         markingMode: 'none'
