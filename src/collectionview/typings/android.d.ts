a381dbd023854343cfd27bec4a8b798145d634c3
Martin Guillon <martin.guillon@akylas.fr>
2022-04-30 20:33:04 +0200

    chore: cleanup typings

---
+++ src/collectionview/typings/android.d.ts
@@ -0,0 +1,66 @@
+declare namespace com {
+    export namespace nativescript {
+        export namespace collectionview {
+            export class GridLayoutManager extends androidx.recyclerview.widget.GridLayoutManager {
+                isScrollEnabled: boolean;
+            }
+            export class PreCachingGridLayoutManager extends GridLayoutManager {
+                setExtraLayoutSpace(space: number);
+            }
+            export class CollectionViewCellHolder extends androidx.recyclerview.widget.RecyclerView.ViewHolder {}
+            export class AdapterInterface {
+                public onCreateViewHolder(param0: globalAndroid.view.ViewGroup, param1: number): CollectionViewCellHolder;
+                public getItemViewType(param0: number): number;
+                public getItemCount(): number;
+                public getItemId(param0: number): number;
+                public onBindViewHolder(param0: CollectionViewCellHolder, param1: number): void;
+                public onViewRecycled(param0: CollectionViewCellHolder): void;
+                constructor(implementation?: {
+                    onCreateViewHolder(param0: globalAndroid.view.ViewGroup, param1: number): CollectionViewCellHolder;
+                    getItemViewType(param0: number): number;
+                    getItemCount(): number;
+                    getItemId(param0: number): number;
+                    onBindViewHolder(param0: CollectionViewCellHolder, param1: number): void;
+                    onViewRecycled(param0: CollectionViewCellHolder): void;
+                });
+            }
+            export class Adapter extends androidx.recyclerview.widget.RecyclerView.Adapter<CollectionViewCellHolder> {
+                adapterInterface: AdapterInterface;
+            }
+            export class RecyclerView extends androidx.recyclerview.widget.RecyclerView {
+                static createRecyclerView(context): RecyclerView;
+                static inflate(context): RecyclerView;
+                sizeChangedListener?: SizeChangedListener;
+            }
+            export class SizeChangedListener {
+                constructor(impl?: { onSizeChanged(w: number, h: number, oldw: number, oldh: number); onMeasure() });
+                onSizeChanged(w: number, h: number, oldw: number, oldh: number);
+                onMeasure();
+            }
+            export class OnScrollListener extends androidx.recyclerview.widget.RecyclerView.OnScrollListener {
+                constructor(listener: OnScrollListener.Listener);
+                setListener(listener: OnScrollListener.Listener);
+            }
+            export namespace OnScrollListener {
+                export class Listener {
+                    constructor(implementation: {
+                        onScrolled(param0: androidx.recyclerview.widget.RecyclerView, param1: number, param2: number): void;
+                        onScrollStateChanged(param0: androidx.recyclerview.widget.RecyclerView, param1: number): void;
+                    });
+                    onScrolled(param0: androidx.recyclerview.widget.RecyclerView, param1: number, param2: number): void;
+                    onScrollStateChanged(param0: androidx.recyclerview.widget.RecyclerView, param1: number): void;
+                }
+            }
+            export class SpanSizeLookup extends androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup {
+                constructor(inter: SpanSizeLookup.Interface);
+                setListener(inter: SpanSizeLookup.Interface);
+            }
+            export namespace SpanSizeLookup {
+                export class Interface {
+                    constructor(implementation: { getSpanSize(param0: number): number });
+                    getSpanSize(param0: number): number;
+                }
+            }
+        }
+    }
+}
--- src/collectionview/typings/arv.d.ts
+++ src/collectionview/typings/arv.d.ts
@@ -4,71 +4,7 @@ import javalangannotationAnnotation = java.lang.annotation.Annotation;
 import javalangRunnable = java.lang.Runnable;
 import javautilList = java.util.List;
 
-declare module com {
-    export module nativescript {
-        export module collectionview {
-            export class GridLayoutManager extends androidx.recyclerview.widget.GridLayoutManager {
-                isScrollEnabled: boolean;
-            }
-            export class PreCachingGridLayoutManager extends GridLayoutManager {
-                setExtraLayoutSpace(space: number);
-            }
-            export class CollectionViewCellHolder extends androidx.recyclerview.widget.RecyclerView.ViewHolder {}
-            export class AdapterInterface {
-                public onCreateViewHolder(param0: globalAndroid.view.ViewGroup, param1: number): CollectionViewCellHolder;
-                public getItemViewType(param0: number): number;
-                public getItemCount(): number;
-                public getItemId(param0: number): number;
-                public onBindViewHolder(param0: CollectionViewCellHolder, param1: number): void;
-                public onViewRecycled(param0: CollectionViewCellHolder): void;
-                constructor(implementation?: {
-                    onCreateViewHolder(param0: globalAndroid.view.ViewGroup, param1: number): CollectionViewCellHolder;
-                    getItemViewType(param0: number): number;
-                    getItemCount(): number;
-                    getItemId(param0: number): number;
-                    onBindViewHolder(param0: CollectionViewCellHolder, param1: number): void;
-                    onViewRecycled(param0: CollectionViewCellHolder): void;
-                });
-            }
-            export class Adapter extends androidx.recyclerview.widget.RecyclerView.Adapter<CollectionViewCellHolder> {
-                adapterInterface: AdapterInterface;
-            }
-            export class RecyclerView extends androidx.recyclerview.widget.RecyclerView {
-                static createRecyclerView(context): RecyclerView;
-                static inflate(context): RecyclerView;
-                sizeChangedListener?: SizeChangedListener;
-            }
-            export class SizeChangedListener {
-                constructor(impl?: { onSizeChanged(w: number, h: number, oldw: number, oldh: number) });
-                onSizeChanged(w: number, h: number, oldw: number, oldh: number);
-            }
-            export class OnScrollListener extends androidx.recyclerview.widget.RecyclerView.OnScrollListener {
-                constructor(listener: OnScrollListener.Listener);
-                setListener(listener: OnScrollListener.Listener);
-            }
-            export namespace OnScrollListener {
-                export class Listener {
-                    constructor(implementation: {
-                        onScrolled(param0: androidx.recyclerview.widget.RecyclerView, param1: number, param2: number): void;
-                        onScrollStateChanged(param0: androidx.recyclerview.widget.RecyclerView, param1: number): void;
-                    });
-                    onScrolled(param0: androidx.recyclerview.widget.RecyclerView, param1: number, param2: number): void;
-                    onScrollStateChanged(param0: androidx.recyclerview.widget.RecyclerView, param1: number): void;
-                }
-            }
-            export class SpanSizeLookup extends androidx.recyclerview.widget.GridLayoutManager.SpanSizeLookup {
-                constructor(inter: SpanSizeLookup.Interface);
-                setListener(inter: SpanSizeLookup.Interface);
-            }
-            export namespace SpanSizeLookup {
-                export class Interface {
-                    constructor(implementation: { getSpanSize(param0: number): number });
-                    getSpanSize(param0: number): number;
-                }
-            }
-        }
-    }
-}
+
 declare module com {
     export module h6ah4i {
         export module android {
