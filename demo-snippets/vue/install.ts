4d552d971dbdf679859b8d446a62e34d4e70aa7b
Martin Guillon <dev@akylas.fr>
2023-04-21 22:05:41 +0200

    demo(vue): resizecell example

--- demo-snippets/vue/install.ts
+++ demo-snippets/vue/install.ts
@@ -10,6 +10,7 @@ import SimpleWaterfall from './SimpleWaterfall.vue';
 import SimpleTemplates from './SimpleTemplates.vue';
 import Reorder from './Reorder.vue';
 import SwipeMenu from './SwipeMenu.vue';
+import ResizeCell from './ResizeCell.vue';
 
 export function installPlugin() {
     Vue.use(CollectionView);
@@ -23,5 +24,6 @@ export const demos = [
     { name: 'Simple Waterfall', path: 'simple-waterfall', component: SimpleWaterfall },
     { name: 'Simple Templates', path: 'simple-templates', component: SimpleTemplates },
     { name: 'Reorder', path: 'reorder', component: Reorder },
-    { name: 'SwipeMenu', path: 'swipe-menu', component: SwipeMenu }
+    { name: 'SwipeMenu', path: 'swipe-menu', component: SwipeMenu },
+    { name: 'ResizeCell', path: 'resize-cell', component: ResizeCell }
 ];
