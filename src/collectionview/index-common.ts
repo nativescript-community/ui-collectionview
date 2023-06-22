63fc82159e53353b5e45091ffcd72e9df56c7360
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2023-06-22 22:57:01 +0100

    Update index-common.ts

--- src/collectionview/index-common.ts
+++ src/collectionview/index-common.ts
@@ -174,8 +174,7 @@ export abstract class CollectionViewBase extends View implements CollectionViewD
     public abstract refreshVisibleItems();
     public abstract isItemAtIndexVisible(index: number);
     public abstract scrollToIndex(index: number, animated: boolean);
-    public abstract scrollToVerticalOffset(value: number, animated?: boolean): any;
-    public abstract scrollToHorizontalOffset(value: number, animated?: boolean): any;
+    public abstract scrollToOffset(value: number, animated?: boolean): any;
 
     protected updateInnerSize() {
         const width = this.getMeasuredWidth();
