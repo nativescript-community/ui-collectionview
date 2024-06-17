f5d9aa79d2a6f5f7e5f9823a85e22e0f85d6aaa4
Martin Guillon <dev@akylas.fr>
2024-06-17 10:39:23 +0200

    fix(ios): working `itemOverlap`

--- src/collectionview/index.ios.ts
+++ src/collectionview/index.ios.ts
@@ -5,6 +5,7 @@ import {
     CoreTypes,
     EventData,
     KeyedTemplate,
+    Length,
     Observable,
     Property,
     ProxyViewContainer,
@@ -988,12 +989,10 @@ export class CollectionView extends CollectionViewBase {
         }
         if (this.itemOverlap) {
             // should we force clipsToBounds? not doing so allows more complex layouts like overlapping
-            cell.clipsToBounds = false;
             // we set zPosition to allow overlap. Should we make it an option?
             cell.layer.zPosition = indexPath.row;
-        } else {
-            cell.clipsToBounds = true;
         }
+        cell.clipsToBounds = true;
         const firstRender = !cell.view;
         if (Trace.isEnabled()) {
             CLog(CLogTypes.log, 'collectionViewCellForItemAtIndexPath', indexPath.row, templateType, !!cell.view, cell);
@@ -1196,6 +1195,32 @@ class UICollectionViewFlowLayoutImpl extends UICollectionViewFlowLayout {
             // owner.clearCachedSize(previousIndexPaths.objectAtIndex(0).row)
         }
         return super.invalidationContextForInteractivelyMovingItemsWithTargetPositionPreviousIndexPathsPreviousPosition(targetIndexPaths, targetPosition, previousIndexPaths, previousPosition);
+    }
+    layoutAttributesForElementsInRect(rect: CGRect) {
+        const attributesArray = super.layoutAttributesForElementsInRect(rect);
+        const owner = this._owner?.get();
+        if (owner?.itemOverlap) {
+            const itemOverlap = owner.itemOverlap;
+            for (let index = 0; index < attributesArray.count; index++) {
+                const attributes = attributesArray.objectAtIndex(index);
+                if (attributes.representedElementCategory === UICollectionElementCategory.Cell) {
+                    const xPosition =
+                        attributes.center.x + Utils.layout.toDeviceIndependentPixels(Length.toDevicePixels(itemOverlap[1], 0) + Length.toDevicePixels(itemOverlap[2], 0)) * attributes.indexPath.row;
+                    const yPosition =
+                        attributes.center.y + Utils.layout.toDeviceIndependentPixels(Length.toDevicePixels(itemOverlap[0], 0) + Length.toDevicePixels(itemOverlap[2], 0)) * attributes.indexPath.row;
+                    attributes.center = CGPointMake(xPosition, yPosition);
+                }
+            }
+        }
+
+        return attributesArray;
+    }
+    shouldInvalidateLayoutForBoundsChange(newBounds: CGRect) {
+        const owner = this._owner?.get();
+        if (owner?.itemOverlap) {
+            return true;
+        }
+        return super.shouldInvalidateLayoutForBoundsChange(newBounds);
     }
 }
 @NativeClass
