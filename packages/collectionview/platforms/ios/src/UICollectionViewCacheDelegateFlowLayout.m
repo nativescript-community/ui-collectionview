be26e3640cdaf6aaa3a56933e1abeadf19a44231
farfromrefuge <dev@akylas.fr>
2025-09-30 15:41:43 +0200

    fix(ios): edge case crash on collection view refresh

--- packages/collectionview/platforms/ios/src/UICollectionViewCacheDelegateFlowLayout.m
+++ packages/collectionview/platforms/ios/src/UICollectionViewCacheDelegateFlowLayout.m
@@ -18,19 +18,21 @@
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
   NSInteger index = indexPath.row;
-  if (index < [self.cachedSizes count]) {
-      NSValue* value =  [self.cachedSizes objectAtIndex:indexPath.row];
+  NSInteger count = [self.cachedSizes count];
+  if (index < count) {
+      NSValue* value =  [self.cachedSizes objectAtIndex:index];
       if (value != nil && !CGSizeEqualToSize([value CGSizeValue], CGSizeZero)) {
           return [value CGSizeValue];
       }
-    CGSize size = [self collectionView:collectionView layout:collectionViewLayout computedSizeForItemAtIndexPath:indexPath];
-    [self.cachedSizes replaceObjectAtIndex:indexPath.row withObject:[NSValue valueWithCGSize:size]];
-    return size;
-  } else {
-    CGSize size = [self collectionView:collectionView layout:collectionViewLayout computedSizeForItemAtIndexPath:indexPath];
-    [self.cachedSizes addObject:[NSValue valueWithCGSize:size]];
-    return size;
   }
+  CGSize size = [self collectionView:collectionView layout:collectionViewLayout computedSizeForItemAtIndexPath:indexPath];
+  count = [self.cachedSizes count];
+  if (index < count) {
+    [self.cachedSizes replaceObjectAtIndex:indexPath.row withObject:[NSValue valueWithCGSize:size]];
+  } else {
+    [self.cachedSizes addObject:[NSValue valueWithCGSize:size]];
+  }
+  return size;
 }
 
 @end
