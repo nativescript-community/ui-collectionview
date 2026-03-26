nativescript-community/ui-collectionview

commit fe649dcf04b4fccce3ac9cddb83fa5dcc37188d8

Add support for rowHeight per template

src/collectionview/index.android.ts
@@ -1,4 +1,4 @@
-﻿import {
+import {
     ChangeType,
     ChangedData,
     ContentView,
@@ -1332,14 +1332,14 @@ export class CollectionView extends CollectionViewBase {
         }
         view.notify({ eventName: CollectionViewBase.bindedEvent });
         let width = this._effectiveColWidth;
-        let height = this._effectiveRowHeight;
+        let height = this.getEffectiveRowHeightForIndex(position);
         if (this._getSpanSize) {
             const spanSize = this._getSpanSize(bindingContext, position);
             const horizontal = this.isHorizontal();
             if (horizontal) {
-                height *= spanSize;
-            } else {
                 width *= spanSize;
+            } else {
+                height *= spanSize;
             }
         }
         if (width || !view.width) {

src/collectionview/angular/collectionview-comp.ts
@@ -414,8 +414,10 @@ export function getItemViewRoot(viewRef: ComponentView, rootLocator: RootLocator
     return rootView;
 }
 
-@Directive({ selector: '[cvTemplateKey]' })
+@Directive({ selector: '[cvTemplateKey],[cvRowHeight]' })
 export class TemplateKeyDirective {
+    private _templateKey: string;
+    private _rowHeight: any;
     constructor(
         private templateRef: TemplateRef<any>,
         @Host() private collectionView: CollectionViewComponent
@@ -426,8 +428,19 @@ export class TemplateKeyDirective {
         if (Trace.isEnabled()) {
             CLog(CLogTypes.info, 'cvTemplateKey: ' + value);
         }
+        this._templateKey = value;
         if (this.collectionView && this.templateRef) {
             this.collectionView.registerTemplate(value.toLowerCase(), this.templateRef);
+            if (this._rowHeight !== undefined) {
+                this.collectionView.nativeElement?.setTemplateRowHeight(value, this._rowHeight);
+            }
         }
+    }
+
+    @Input()
+    set cvRowHeight(value: any) {
+        this._rowHeight = value;
+        const key = this._templateKey || 'default';
+        this.collectionView?.nativeElement?.setTemplateRowHeight(key, value);
     }
 }

src/collectionview/index-common.ts
@@ -159,9 +159,11 @@ export abstract class CollectionViewBase extends View implements CollectionViewD
 
     protected _itemTemplatesInternal: Map<string, KeyedTemplate>;
     protected _defaultTemplate: KeyedTemplate;
+    protected _templateRowHeights: Map<string, CoreTypes.PercentLengthType>;
 
     constructor() {
         super();
+        this._templateRowHeights = new Map();
         this._defaultTemplate = {
             key: 'default',
             createView: () => {
@@ -173,6 +175,28 @@ export abstract class CollectionViewBase extends View implements CollectionViewD
         };
         this._itemTemplatesInternal = new Map();
         this._itemTemplatesInternal.set(this._defaultTemplate.key, this._defaultTemplate);
+    }
+
+    public setTemplateRowHeight(key: string, value: any) {
+        const templateKey = (key || this._defaultTemplate.key).toLowerCase();
+        if (value === undefined || value === null || value === '') {
+            this._templateRowHeights.delete(templateKey);
+        } else {
+            this._templateRowHeights.set(templateKey, PercentLength.parse(value));
+        }
+    }
+
+    public getEffectiveRowHeightForIndex(index: number) {
+        const selector = this._itemTemplateSelector;
+        let templateKey = this._defaultTemplate.key;
+        if (selector) {
+            templateKey = selector.call(this, this.getItemAtIndex(index), index, this.items);
+        }
+        const perTemplate = this._templateRowHeights.get((templateKey || this._defaultTemplate.key).toLowerCase());
+        if (perTemplate) {
+            return toDevicePixels(perTemplate as any, autoEffectiveRowHeight, this._innerHeight);
+        }
+        return this._effectiveRowHeight;
     }
     notifyForItemAtIndex(eventName: string, view: View, index: number, bindingContext?: any, native?: any) {
         throw new Error('Method not implemented.');

src/collectionview/index.ios.ts
@@ -492,7 +492,7 @@ export class CollectionView extends CollectionViewBase {
                 // if we use fixed col and row size we want a delegate
                 // without collectionViewLayoutSizeForItemAtIndexPath
                 // because it is not needed and faster
-                if (this._effectiveColWidth && this._effectiveRowHeight) {
+                if (this._effectiveColWidth && this._effectiveRowHeight && (!this._templateRowHeights || this._templateRowHeights.size === 0)) {
                     this._delegate = UICollectionViewDelegateFixedSizeImpl.initWithOwner(this);
                 } else {
                     this._delegate = UICollectionViewDelegateImpl.initWithOwner(this);
@@ -514,7 +514,7 @@ export class CollectionView extends CollectionViewBase {
     updateRowColSize() {
         const layoutView = this.nativeViewProtected?.collectionViewLayout;
         if (layoutView instanceof UICollectionViewFlowLayout) {
-            if (this._effectiveRowHeight && this._effectiveColWidth) {
+            if (this._effectiveRowHeight && this._effectiveColWidth && (!this._templateRowHeights || this._templateRowHeights.size === 0)) {
                 layoutView.itemSize = CGSizeMake(Utils.layout.toDeviceIndependentPixels(this._effectiveColWidth), Utils.layout.toDeviceIndependentPixels(this._effectiveRowHeight));
             } else if (this.estimatedItemSize && !this.autoSize) {
                 layoutView.estimatedItemSize = CGSizeMake(Utils.layout.toDeviceIndependentPixels(this._effectiveColWidth), Utils.layout.toDeviceIndependentPixels(this._effectiveRowHeight));
@@ -987,7 +987,7 @@ export class CollectionView extends CollectionViewBase {
         // CLog(CLogTypes.log, 'getCellSize', index, result, this._effectiveColWidth, this._effectiveRowHeight, this.getMeasuredWidth(), this.getMeasuredHeight());
         if (!result) {
             let width = this._effectiveColWidth;
-            let height = this._effectiveRowHeight;
+            let height = this.getEffectiveRowHeightForIndex(index);
             if (this.spanSize) {
                 const dataItem = this.getItemAtIndex(index);
                 const spanSize = this.spanSize(dataItem, index);
@@ -1019,7 +1019,7 @@ export class CollectionView extends CollectionViewBase {
     private measureCell(cell: CollectionViewCell, cellView: View, position: number): [number, number] {
         if (cellView) {
             let width = this._effectiveColWidth;
-            let height = this._effectiveRowHeight;
+            let height = this.getEffectiveRowHeightForIndex(position);
             const horizontal = this.isHorizontal();
             if (this.spanSize) {
                 const dataItem = this.getItemAtIndex(position);

commit d0c55a8145f73198c49f1402880512c020a943d5

fix(android): swap width/height spanSize multiplication in onBindViewHolder
Commit fe649dc ("Add support for rowHeight per template") accidentally
swapped the width/height multiplication when applying spanSize on Android.

For a vertical CollectionView, spanSize should multiply the **width**
(item spans across columns). Instead, it was multiplying the height,
causing items with spanSize > 1 to render taller but only occupy one
column.

The iOS implementation (index.ios.ts) was not affected and has the
correct logic:
- vertical: width *= spanSize
- horizontal: height *= spanSize

This fix aligns the Android behavior with iOS.

src/collectionview/index.android.ts
@@ -1371,9 +1371,9 @@ export class CollectionView extends CollectionViewBase {
             const spanSize = this._getSpanSize(bindingContext, position);
             const horizontal = this.isHorizontal();
             if (horizontal) {
-                width *= spanSize;
-            } else {
                 height *= spanSize;
+            } else {
+                width *= spanSize;
             }
         }
         if (width || !view.width) {